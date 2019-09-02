9f40969c0632296ed7b486b4d65de2a53e518a6f
Amod Mulay <AmodMulay1@gmail.com>
2019-09-01 23:22:33 -0400

    second pass at knapsack with dict for cache

--- python/knapsack/knapsack.py
+++ python/knapsack/knapsack.py
@@ -1,16 +1,22 @@
+from copy import deepcopy
+
+
 def maximum_value(maximum_weight, items):
-    if len(items) == 0:
-        return 0
-    min_weight = min([item["weight"] for item in items ])
-    if min_weight > maximum_weight:
-        return 0
-    i = 0
-    max_val = 0
-    while i<2**len(items):
-        seq = format(i, '0'+str(len(items))+'b')  # represent i as binary string of length len(items)
-        total_weight = sum([items[i]["weight"] for i in range(len(items)) if seq[i] == '1' ])
-        total_value = sum([items[i]["value"] for i in range(len(items)) if seq[i] == '1' ])
-        if total_weight <= maximum_weight:
-            max_val = max(max_val, total_value)
-        i += 1
-    return max_val
+    cache = dict()
+    def inner_max_value(maximum_weight, items, cache={}):
+        N = len(items)
+        if N == 0 or maximum_weight == 0:
+            return 0
+        if (maximum_weight, N) in cache:
+            return cache[(maximum_weight, N)]
+        next_items = deepcopy(items)
+        item = next_items.pop()
+        if item["weight"] > maximum_weight:
+            cache[(maximum_weight, N)] = inner_max_value(maximum_weight, next_items, cache)
+            return cache[(maximum_weight, N)]
+        cache[(maximum_weight, N)] = max([
+            inner_max_value(maximum_weight, next_items, cache),
+            item["value"] + inner_max_value(maximum_weight-item["weight"], next_items, cache)
+        ])
+        return cache[(maximum_weight, N)]
+    return inner_max_value(maximum_weight, items, cache)
