4b78a177990e7ffd3496367414a295a237465a91
Amod Mulay <AmodMulay1@gmail.com>
2019-09-01 23:30:54 -0400

    optimize knapsack to prevent deepcopies

--- python/knapsack/knapsack.py
+++ python/knapsack/knapsack.py
@@ -1,22 +1,21 @@
-from copy import deepcopy
-
-
 def maximum_value(maximum_weight, items):
     cache = dict()
-    def inner_max_value(maximum_weight, items, cache={}):
-        N = len(items)
+    def inner_max_value(maximum_weight, N, cache={}):
         if N == 0 or maximum_weight == 0:
             return 0
         if (maximum_weight, N) in cache:
             return cache[(maximum_weight, N)]
-        next_items = deepcopy(items)
-        item = next_items.pop()
+
+        item = items[N-1]
         if item["weight"] > maximum_weight:
-            cache[(maximum_weight, N)] = inner_max_value(maximum_weight, next_items, cache)
+            cache[(maximum_weight, N)] = inner_max_value(maximum_weight, N-1, cache)
             return cache[(maximum_weight, N)]
+
         cache[(maximum_weight, N)] = max([
-            inner_max_value(maximum_weight, next_items, cache),
-            item["value"] + inner_max_value(maximum_weight-item["weight"], next_items, cache)
+            inner_max_value(maximum_weight, N-1, cache),
+            item["value"] + inner_max_value(maximum_weight-item["weight"], N-1, cache)
         ])
+
         return cache[(maximum_weight, N)]
-    return inner_max_value(maximum_weight, items, cache)
+
+    return inner_max_value(maximum_weight, len(items), cache)
