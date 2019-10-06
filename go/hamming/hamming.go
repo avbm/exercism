7c1952f190c35a3def778f9b7686a53f98601767
Amod Mulay <amodmulay1@gmail.com>
2019-10-06 14:16:15 -0400

    error generation update for hamming based on review

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,12 +1,12 @@
 // Package hamming calculates the Hamming difference between two DNA strands.
 package hamming
 
-import "fmt"
+import "errors"
 
 // Distance returns the Hamming difference between 2 strings representing DNA strands.
 func Distance(a, b string) (int, error) {
 	if len(a) != len(b) {
-		return 0, fmt.Errorf("Sequence lengths don't match")
+		return 0, errors.New("sequence lengths don't match")
 	}
 
 	count := 0
